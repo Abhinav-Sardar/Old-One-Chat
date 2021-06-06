3c36cbda02580a47381245922494176f43f89771
Abhinav-Sardar <abhinavsardar.podar@gmail.com>
2021-06-06 22:01:44 +0530

    Added Basic CHat styles

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,33 +1,40 @@
-import React, { useEffect , useState } from 'react'
+import React, { useEffect , useState , createContext } from 'react'
 import io from 'socket.io-client' ;
 import QueryString from 'qs'; 
 import NotfoundPage from '../NotfoundPage/NotfoundPage' ; 
 import styles from './Chat.module.css' ; 
 import {Sidebar , MainChat} from '../Chat-Components/Components'
+export const nameContext = createContext() ;
+export const roomContext = createContext() ;
+export const messagesContext = createContext() ;
 const Chat = () => {
     const [isValidURL , setIsValidURL] = useState(false) ;
     const [name , setName] = useState('') ;
     const [room , setRoom] = useState('') ;
     const [messages , setMessages] = useState([]) ; 
-
+    
+    
+    
+    
 
     const RealChat = () => {
         const socket = io('http://localhost:1919/') ;
         return (
-            <React.Fragment>
-                <div className = {styles.page}>
-                    <Sidebar styles = {styles}/>
-                    <MainChat styles = {styles}/>
-                </div>
-            </React.Fragment>
+            <nameContext.Provider value = {name}>
+                <roomContext.Provider value = {room}>
+                    <messagesContext.Provider value = {messages}>
+                        <React.Fragment>
+                    <div className = {styles.page}>
+                        <Sidebar styles = {styles}/>
+                        <MainChat styles = {styles}/>
+                    </div>
+                </React.Fragment>
+                    </messagesContext.Provider>
+                </roomContext.Provider>
+            </nameContext.Provider>
         )
     }
-
-
-
-
-
-
+    
     useEffect(() => {
         const {name , room} = QueryString.parse(window.location.href, {
             ignoreQueryPrefix:true ,
